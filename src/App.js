1efee4dbd37068c011cc4b3c7451fcf8784ebdb6
sunha0 <763625521@qq.com>
2024-03-23 16:06:42 +0800

    react 数据引用流程

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// 项目的根组件
+// App -> index.js -> public/index.html(root)
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Let's do it!
-        </p>
-        <a
-          className="App-link"
-          href="https://www.baidu.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn 
-        </a>
-      </header>
+        This is React
     </div>
   );
 }
