343cdfdf6fc5019872098aa0efabfd72939d45d4
sunha0 <763625521@qq.com>
2024-03-24 20:10:32 +0800

    userState 修改状态

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-
-const Button = () => {
-  return <button>click me</button>
-}
+// userState 实现一个计数器按钮 
 
 function App() {
-  
- 
 
-  return (
-    <div className="App">
+// 1、调用useState 添加一个状态变量
+// 2、count 状态变量
+// 3、setCount 修改状态变量的方法
+ const [count,setCount] = useState(0)
+
+
+// 点击事件回调
+const handleClick = () =>{
+
+  // 作用: 1、用传入的新值修改count
+  // 2、 重新使用新的count 渲染UI 
+
+  setCount(count + 1)
+
+}
+
+
+// 修改对象状态
+
+const [form,setForm] = useState({name:"jack"})
+
+
+const handleForm = () => {
+
+    setForm(
+      {
+      ...form,
+      name:"lucy"
+      }
       
-        <img src={logo} className="App-logo" alt="logo" />
+      )
+}
 
-        <Button></Button>
+
+  return (  
+    <div className="App">
+
+        <button onClick={handleClick}>{count}</button>
+
+        <button onClick={handleForm} >选择</button>
+
+        <input  placeholder={form.name}></input>
+
 
     </div>
   );
